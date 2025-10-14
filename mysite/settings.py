6b3d5124831b16515b85efbd7f7abdccea3f7779
oksankah <oksana.harvasiuk@gmail.com>
2025-10-14 08:55:05 +0300

    Blog page

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,7 +121,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'app_blog', 'static'),
 ]
 
 MEDIA_URL = '/media/'
